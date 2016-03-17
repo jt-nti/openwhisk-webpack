5ce5a12a8f6251dcc177d99290ad59acb331ded6
Jonathan Kaufman <jondakauf@gmail.com>
2016-03-17 09:26:00 -0400

    initial commit - working multifile webpack openwhisk action

---
+++ index.js
@@ -0,0 +1,29 @@
+//-------------------------------------------------------------------------------
+// Copyright IBM Corp. 2016
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-------------------------------------------------------------------------------
+
+import { getUserRepos, getRepoLanguages } from './githubApi';
+import Promise from 'bluebird';
+
+global.main = ({ user }) => {
+  getUserRepos(user).then(repos =>
+    Promise.map(repos, ({ name }) => getRepoLanguages(user, name))
+  ).then(languageArr =>
+    whisk.done({ languageArr })
+  ).catch(e =>
+    whisk.done(e)
+  );
+  return whisk.async();
+};
